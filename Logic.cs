d4ef763e26b43bb0b0619a094f30a63a6960b190
Gustaf Malmsten <gustaf.malmsten@gmail.com>
2021-10-19 20:39:22 +0200

    Lade till en sökfunktion, som sorterar i fallande ordning efter antalet träffar, fixade en bug och lade till en testfunktion

--- Logic.cs
+++ Logic.cs
@@ -22,6 +22,9 @@ namespace Assignment2
                 Console.WriteLine(item.text);
             }
             Console.ReadLine();
+            // Avkommentera koden nedan för att köra metoden ReadDocument 3 gånger, och sedan lista i ordning efter söktermen "och"
+            // test();
+            // Console.ReadLine();
 
         }
 
@@ -37,7 +40,7 @@ namespace Assignment2
             {
                 Console.WriteLine("Please provide an exact filepath for .txt file you want to read into the program:\n\n");
                 string path = Console.ReadLine();
-                if (String.IsNullOrEmpty(path) || path.Length < 4 || !path.Substring(path[^4]).Equals(".txt") || !File.Exists(path) || new FileInfo(path).Length == 0)
+                if (String.IsNullOrEmpty(path) || path.Length < 5 || !path.Substring(path.Length - 4).Equals(".txt") || !File.Exists(path) || new FileInfo(path).Length == 0)
                 {
                     Console.WriteLine("Please enter a valid, non-empty text file.");
                 }
@@ -75,6 +78,52 @@ namespace Assignment2
             Console.ReadLine();
             Console.Clear();
             return result;
+        }
+        /// <summary>
+        /// Searches for a word in the loaded documents. Returns the number of matches in the documents, and in descending order.
+        /// </summary>
+        /// <param name="searchString">The string to search for</param>
+        /// <returns>A list of files and matches in descending order</returns>
+        public static List<(string filename, int count)> Search(string searchString)
+        {
+            List<(string filename, int count)> result = new();
+
+            foreach (var item in Texts)
+            {
+                int count = 0;
+                string name = item.filename.Substring(item.filename.LastIndexOf("\\") + 1);
+                string[] arr = item.text.Split(" ");
+                for (int i = 0; i < arr.Length; i++)
+                {
+                    if (arr[i].Equals(searchString))
+                    {
+                        count++;
+                    }
+                }
+                result.Add((name, count));
+            }
+            result.Sort((x, y) => y.count.CompareTo(x.count));
+            return result;
+        }
+
+        /// <summary>
+        /// Testfunktion
+        /// </summary>
+        public static void test()
+        {
+            Console.Clear();
+            for (int i = 0; i < 3; i++)
+            {
+                Texts.Add(ReadDocument());
+            }
+            var list = Search("och");
+
+            foreach (var item in list)
+            {
+                Console.WriteLine(item);
+            }
+
+
         }
     }
 }
